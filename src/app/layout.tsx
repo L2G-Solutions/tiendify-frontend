1f59d19314db96128ad1ab0f50f29d8c11838833
Jose David Gómez <jose.gomez14@udea.edu.co>
2024-10-28 17:16:13 -0500

    feat: :wrench: Add Providers component for global state management of NextUI, React Query and Sonner Toast
    Co-authored-by: daniel-lujan <daniel.lujan@udea.edu.co>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "@/providers/providers";
 
 export const metadata: Metadata = {
   title: "Tiendify",
@@ -24,7 +25,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased`}
       >
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
---
+++ src/providers/providers.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Toaster } from 'sonner';
+
+import { NextUIProvider } from '@nextui-org/react';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Toaster
+        theme="dark"
+        toastOptions={{
+          style: {
+            color: '#171F2C',
+            background: '#70A6FF',
+            borderColor: '#212C3F',
+            fontSize: '1.075rem',
+            gap: '0.75rem',
+          },
+        }}
+      />
+      <NextUIProvider>{children}</NextUIProvider>
+    </QueryClientProvider>
+  );
+}
