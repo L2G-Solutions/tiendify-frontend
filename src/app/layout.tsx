61858dce49a34f1c1379092a97d04b87fe916e18
daniel-lujan <daniel.lujan@udea.edu.co>
2024-10-30 13:41:09 -0500

    feat: :sparkles: Add Footer UI component to global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Providers } from '@/providers/providers';
 import NavBar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
   title: 'Tiendify',
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased flex flex-col min-h-screen`}>
         <NavBar />
         <Providers>{children}</Providers>
+        <Footer />
       </body>
     </html>
   );
