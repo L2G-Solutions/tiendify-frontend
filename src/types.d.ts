c3b4ce45609b10ce70f26aaa3e6798d4a695daa0
Jose David Gómez <jose.gomez14@udea.edu.co>
2024-11-03 14:14:22 -0500

    feat: :sparkles: Add TProduct type definition for product management

---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+type TProduct = {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  thumbnailImg: string;
+  mediaFiles: string[];
+  stock: number;
+  categories: string[];
+  createdAt: string;
+};
