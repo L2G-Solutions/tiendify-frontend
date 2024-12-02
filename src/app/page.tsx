f45d06eebafa91c7315c9f24b6aac24d19a6e9e2
Jose David Gómez <jose.gomez14@udea.edu.co>
2024-12-02 17:21:52 -0500

    feat: :sparkles: Add features section with FeatureCard components to the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
+import FeatureCard from '@/components/landing/FeatureCard';
 import LandingHeroSection from '@/components/landing/HeroSection';
 import ShadowElipse from '@/components/ShadowElipse';
+
+import { FEATURES } from '@/constants/landingInfo';
 
 export default function Home() {
   return (
     <>
       <LandingHeroSection />
+      <h2 id="about" className="text-6xl text-primary font-bold text-center py-12">
+        What we offer
+      </h2>
+      <div className='flex justify-center'>
+        <section className="px-8 lg:px-32 grid grid-cols-1 gap-12 py-8 md:grid-cols-2 lg:grid-cols-3 auto-rows-min">
+          {FEATURES.map((feature, index) => (
+            <FeatureCard
+              key={index}
+              title={feature.title}
+              description={feature.description}
+              icon={feature.illustrationPath}
+              redirectRoute={feature.redirectRoute}
+            />
+          ))}
+        </section>
+      </div>
       <ShadowElipse
         className="absolute -top-[50px] -left-[150px]"
         size="674px"
@@ -19,6 +38,14 @@ export default function Home() {
         internalColor="rgba(147, 83, 211, 0.80)"
         externalColor="rgba(125, 78, 255, 0.00)"
         internalStopColor="#9353D3"
+        externalStopColor="#7D4EFF"
+      />
+      <ShadowElipse
+        className="absolute top-[100vh] -left-[500px]"
+        size="674px"
+        internalColor="rgba(0, 111, 238, 0.40)"
+        externalColor="rgba(125, 78, 255, 0.00)"
+        internalStopColor="#006FEE"
         externalStopColor="#7D4EFF"
       />
     </>
---
+++ src/constants/landingInfo.ts
@@ -0,0 +1,44 @@
+export const FEATURES = [
+  {
+    title: 'Make your own view',
+    description:
+      'Unleash your creativity with our powerful API! Build your own unique front-end and customize every aspect of your eCommerce experience.',
+    illustrationPath: '/illustrations/features/details.svg',
+    redirectRoute: '/docs',
+  },
+  {
+    title: 'Sell products globally',
+    description:
+      'Expand your reach with our multi-language and multi-currency features! Sell to anyone, anywhere in the world.',
+    illustrationPath: '/illustrations/features/connected-world.svg',
+    redirectRoute: '/docs',
+  },
+  {
+    title: 'Secure payments',
+    description:
+      'Rest easy knowing that your payments are secure with our PCI-compliant payment gateway. We support all major credit cards and PayPal.',
+    illustrationPath: '/illustrations/features/online-payments.svg',
+    redirectRoute: '/docs',
+  },
+  {
+    title: '24/7 online support',
+    description:
+      'Our team of experts is here to help you with any questions you may have. Get in touch with us anytime, day or night!',
+    illustrationPath: '/illustrations/features/active-support.svg',
+    redirectRoute: '/docs',
+  },
+  {
+    title: 'Powerful analytics',
+    description:
+      'Track your store’s performance with our powerful analytics tools. Monitor your sales, customer behavior, and more to make informed decisions.',
+    illustrationPath: '/illustrations/features/real-time-analytics.svg',
+    redirectRoute: '/docs',
+  },
+  {
+    title: 'Easy integrations',
+    description:
+      'Easily connect your store with your favorite tools and services. We offer seamless integrations with popular platforms like Mailchimp, Google Analytics, and more.',
+    illustrationPath: '/illustrations/features/online-connection.svg',
+    redirectRoute: '/docs',
+  },
+];
