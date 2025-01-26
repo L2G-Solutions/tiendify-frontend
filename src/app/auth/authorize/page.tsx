5f15195e0947026b9412e11cfed78ae269e2c24c
daniel-lujan <daniel.lujan@udea.edu.co>
2025-01-25 21:55:08 -0500

    fix: :bug: Refetch user session upon authorization

--- src/app/auth/authorize/page.tsx
+++ src/app/auth/authorize/page.tsx
@@ -6,6 +6,7 @@ import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
 import useCurentUrl from '@/hooks/useCurentUrl';
 import { Spinner } from '@nextui-org/react';
+import { useAuth } from '@/hooks/useAuth';
 
 export default function AuthorizePage() {
   return (
@@ -18,6 +19,8 @@ export default function AuthorizePage() {
 const AuthorizeLoginComponent = () => {
   const navigate = useRouter().push;
 
+  const { refetch } = useAuth();
+
   const searchParams = useSearchParams();
   const params = new URLSearchParams(searchParams);
 
@@ -29,11 +32,11 @@ const AuthorizeLoginComponent = () => {
     mutationFn: () => authorizeLogin(params, LOGIN_REDIRECT_URL),
     onSuccess: () => {
       const next_url = searchParams.get('next');
+      refetch();
       if (next_url) {
         navigate(next_url);
         return;
       }
-
       navigate('/dashboard');
     },
   });
