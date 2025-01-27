323874be35c2c4c710f26e72d5e381c6a334a3c1
Jose David Gómez <jose.gomez14@udea.edu.co>
2025-01-27 15:53:40 -0500

    feat: :sparkles: Add documentation link constants and utility function for dynamic URL generation

---
+++ src/constants/docLinks.ts
@@ -0,0 +1,13 @@
+const BASE_URL = process.env.NEXT_PUBLIC_DOCS_URL || 'http://localhost:3001';
+
+const DOCK_PATHS = {
+  '': '',
+  docs: 'docs/intro',
+  blog: 'blog',
+  infraestructure: 'docs/infrastructure',
+  auth: 'store-api/authentication',
+  adminAuth: 'docs/store-api/admin%20access',
+  apiReference: 'api-reference',
+};
+
+export const getDocUrl = (path: keyof typeof DOCK_PATHS) => `${BASE_URL}/${DOCK_PATHS[path]}`;
