6587650596e4009b5ab7486ea97bbd226ca2668d
Jose David Gómez <jose.gomez14@udea.edu.co>
2025-01-27 15:51:23 -0500

    feat: :sparkles: Enhance DashboardMenu with badge support for menu links

--- src/components/dashboard/DashboardMenu.tsx
+++ src/components/dashboard/DashboardMenu.tsx
@@ -1,3 +1,4 @@
+import { Chip } from '@nextui-org/react';
 import {
   IconAddressBook,
   IconApi,
@@ -17,11 +18,11 @@ export const DashboardMenu = () => {
         <IconBuildingStore stroke={1} />
         My shop
       </MenuLink>
-      <MenuLink href="/dashboard/products">
+      <MenuLink href="/dashboard/products" badge="updated">
         <IconShoppingBag stroke={1} />
         Products
       </MenuLink>
-      <MenuLink href="/dashboard/orders">
+      <MenuLink href="/dashboard/orders" badge="beta">
         <IconTruckDelivery stroke={1} />
         Orders
       </MenuLink>
@@ -29,19 +30,19 @@ export const DashboardMenu = () => {
         <IconUsers stroke={1} />
         Customers
       </MenuLink>
-      <MenuLink href="/dashboard/billing">
+      <MenuLink href="/dashboard/" badge="coming soon">
         <IconReceipt2 stroke={1} />
         Billing
       </MenuLink>
-      <MenuLink href="/dashboard/account">
+      <MenuLink href="/dashboard/account" badge="coming soon">
         <IconAddressBook stroke={1} />
         Account
       </MenuLink>
-      <MenuLink href="/dashboard/api-integration">
+      <MenuLink href="/dashboard/api-integration" badge="new">
         <IconApi stroke={1} />
         API Integration
       </MenuLink>
-      <MenuLink href="/dashboard/settings">
+      <MenuLink href="/dashboard/settings" badge="coming soon">
         <IconSettings stroke={1} />
         Settings
       </MenuLink>
@@ -52,15 +53,34 @@ export const DashboardMenu = () => {
 type MenuLinkProps = {
   href: string;
   children: React.ReactNode;
+  badge?: 'new' | 'updated' | 'coming soon' | 'beta';
 };
 
-const MenuLink = ({ href, children }: MenuLinkProps) => {
+const MenuLink = ({ href, children, badge }: MenuLinkProps) => {
   return (
     <Link
       href={href}
       className="inline-flex items-center gap-4 px-28 py-4 hover:bg-primary-100 transition-colors hover:text-primary"
     >
       {children}
+      {badge && (
+        <Chip
+          color={
+            badge === 'new'
+              ? 'success'
+              : badge === 'updated'
+              ? 'secondary'
+              : badge === 'coming soon'
+              ? 'default'
+              : 'warning'
+          }
+          size="sm"
+          variant="flat"
+          className="capitalize"
+        >
+          {badge}
+        </Chip>
+      )}
     </Link>
   );
 };
